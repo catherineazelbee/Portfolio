5a96d246a82add4a828ca29a24cda5f6f0228eaa
catherineazelbee <52515661+catherineazelbee@users.noreply.github.com>
2025-05-12 21:09:47 -0400

    Bug Illustration Page Fixes

--- illustration.js
+++ illustration.js
@@ -8,8 +8,8 @@ document.addEventListener("DOMContentLoaded", () => {
         "illust_Comic.png": "Comic strip created for Color and Composition final project. Created using Adobe Photoshop 2023.",
         "illust_MaxBlaire.png": "Illustration of original characters Max and Blair. Created in Adobe Photoshop 2025.",
         "illust_mindysFriends.png": "Illustration of original characters (from left: Jacob, Mindy, Alia). Created in Adobe Photoshop 2025.",
-        "illust_princess.gif": "Princess Flora character design and illustration created for Game Jam: Slice of Jam 2025. Created using Adopbe Photoshop 2025.",
-        "illust_knight.gif": "Knight Mars character design and illustration created for Game Jam: Slice of Jam 2025. Created using Adopbe Photoshop 2025.",
+        "illust_princess.gif": "Princess Flora character design and illustration created for Game Jam: Slice of Jam 2025. Created using Adobe Photoshop 2025.",
+        "illust_knight.gif": "Knight Mars character design and illustration created for Game Jam: Slice of Jam 2025. Created using Adobe Photoshop 2025.",
         "illust_phoneCall.gif": "Animated GIF illustration. Created using Adobe Photoshop 2025."
     };
 
